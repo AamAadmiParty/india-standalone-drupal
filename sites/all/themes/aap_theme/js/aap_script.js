222930d21cd8a5e9d98714bedb3fa13f053f3430
Sumeet Pareek <sumeet.pareek@innoraft.com>
2014-01-29 14:42:56 +0530

    Adding in CSS for header buttons
    references AamAadmiParty/india-standalone-drupal#77

---
+++ sites/all/themes/aap_theme/js/aap_script.js
@@ -0,0 +1,17 @@
+(function ($, Drupal, window, document, undefined) {
+  Drupal.behaviors.aap_republic_day = {
+      attach: function(context) {
+      	/*
+        republic_image = $('#republic-day-constitution-image').detach();
+        republic_explore = $('#republic-day-explore-link').detach();
+        $('body.section-republic-day #aap-panel-variant-default').before(republic_image);
+        $('body.section-republic-day #aap-panel-variant-default').before(republic_explore);
+        */
+      }
+  } 
+}
+)(jQuery, Drupal, this, this.document); 
+
+
+
+
