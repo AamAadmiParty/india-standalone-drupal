437804f27774a42e53ea33ad3fb7b2cfefd07233
tkuldeep <tkuldeep.singh@innoraft.com>
2014-02-05 19:31:30 +0530

    Make pointer arrow and create default popup
     reference -> AamAadmiParty/india-standalone-drupal#115

--- sites/all/themes/aap_theme/js/svg.js
+++ sites/all/themes/aap_theme/js/svg.js
@@ -5,15 +5,23 @@
       $("path").click(function() {
         $(".view-svg-maps").append("<div id=popup_content></div>");
         var clickedState = $(this).attr('id');
-        getData(clickedState);
+
+        // if user click on foreign or water region, then do not show popup
+        if (clickedState != 'foreign_1_' && clickedState != 'water_1_') {
+          getData(clickedState);
+        };
       });
 
       function getData(clickedState) {
         var state_div = $( "div.gmap-state:contains("+clickedState+")" );
         if (state_div.length != 0) {
-           var title = state_div.siblings('.gmap-title').html();
-        var data = state_div.siblings('.gmap-body').html();
-        $( "#popup_content" ).html(data).dialog({ title: title,minWidth: 300,maxWidth:400});
+          var title = state_div.siblings('.gmap-title').html();
+          var data = state_div.siblings('.gmap-body').html();
+          $( "#popup_content" ).html(data).dialog({ title: title});
+        }
+        // if content is not added
+        else {
+          $( "#popup_content" ).html("Data yet to be added").dialog({ title: 'Data yet to be added'});
         }
       }
     }
