48881a432408776dbbb4ada34799aefce216d4ac
Sumeet Pareek <sumeet.pareek@innoraft.com>
2014-02-06 17:49:30 +0530

    Fixed the width of sticky donation widget
    references AamAadmiParty/india-standalone-drupal#120

--- sites/all/themes/aap_theme/js/fix_donation_widget_right_sidebar.js
+++ sites/all/themes/aap_theme/js/fix_donation_widget_right_sidebar.js
@@ -2,6 +2,10 @@
   Drupal.behaviors.fix_donation_widget_right_sidebar = {
     attach: function(context) {
       var donation_pos = $( ".aap-donation-right-class" ).offset().top;
+      // compute the original width of right side donation widget and keep it that way
+      // basically set it explicitly so that it remains in position when made 'fixed' later
+      var donation_right_widget_width = $('.aap-donation-right-class').width();
+      $('.aap-donation-right-class').width(donation_right_widget_width);
       $(window).scroll(function() { //when window is scrolled
         $pos = donation_pos - $(window).scrollTop();
         if ($pos < 65) {
